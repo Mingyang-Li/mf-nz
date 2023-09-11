e6951ea0f318d2df44cde40ef1010f5d397a934e
Mingyang-Li <mingyangli118@gmail.com>
2023-09-11 22:29:25 +1200

    fix: formatting check

--- src/hooks/use-screen-size.ts
+++ src/hooks/use-screen-size.ts
@@ -5,12 +5,12 @@ export const useScreenSize = () => {
   let initDimensions = {
     width: 0,
     height: 0,
-  }
-  if (typeof window !== "undefined") {
+  };
+  if (typeof window !== 'undefined') {
     initDimensions = {
       width: window.innerWidth,
       height: window.innerHeight,
-    }
+    };
   }
   const [screenSize, setScreenSize] = useState(initDimensions);
 
