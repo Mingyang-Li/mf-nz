207593b30ee436c8444904c78b1fd28049e4616c
Mingyang-Li <mingyangli118@gmail.com>
2023-09-16 13:15:49 +1200

    chore: formatting

--- src/components/nav-gpt.tsx
+++ src/components/nav-gpt.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const HeaderNavMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,25 +19,28 @@ const HeaderNavMenu: React.FC = () => {
         onClick={toggleMenu}
       >
         <span
-          className={`block w-6 h-1 mb-1 bg-gray-700 transition-transform transform-gpu duration-300 ${isOpen ? "rotate-45 translate-y-1" : "rotate-0"
-            }`}
+          className={`block w-6 h-1 mb-1 bg-gray-700 transition-transform transform-gpu duration-300 ${
+            isOpen ? 'rotate-45 translate-y-1' : 'rotate-0'
+          }`}
         ></span>
         <span
-          className={`block w-6 h-1 mb-1 bg-gray-700 transition-opacity ${isOpen ? "opacity-0" : "opacity-100"
-            }`}
+          className={`block w-6 h-1 mb-1 bg-gray-700 transition-opacity ${
+            isOpen ? 'opacity-0' : 'opacity-100'
+          }`}
         ></span>
         <span
-          className={`block w-6 h-1 bg-gray-700 transition-transform transform-gpu duration-300 ${isOpen ? "-rotate-45 -translate-y-1" : "rotate-0"
-            }`}
+          className={`block w-6 h-1 bg-gray-700 transition-transform transform-gpu duration-300 ${
+            isOpen ? '-rotate-45 -translate-y-1' : 'rotate-0'
+          }`}
         ></span>
       </button>
       <AnimatePresence>
         {isOpen && (
           <>
             <motion.div
-              initial={{ x: "100%" }}
+              initial={{ x: '100%' }}
               animate={{ x: 0 }}
-              exit={{ x: "100%" }}
+              exit={{ x: '100%' }}
               transition={{ duration: 0.3 }}
               className="fixed top-0 right-0 h-screen w-3/4 bg-white z-50 p-8"
             >
